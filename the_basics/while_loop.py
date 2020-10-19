9c5488b2177965faae05bcc11723ed6f99acde88
Michael Chan <michchandev@gmail.com>
2020-10-19 17:38:23 +0800

    Add while_loop.py

---
+++ the_basics/while_loop.py
@@ -0,0 +1,14 @@
+username = ''
+
+while username != 'pypy':
+    username = input('1. Enter username: ')
+
+# break and continue
+while True:
+    username = input('2. Enter username: ')
+    if username == 'pypy':
+        break
+    else:
+        continue
+
+
