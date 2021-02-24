3c911e0666bb29d8443ad548b8034c631a79eded
Michael Chan <michchandev@gmail.com>
2021-02-24 23:15:46 +0800

    Add pgsql_script

--- 5_db/pgsql_script.py
+++ 5_db/pgsql_script.py
@@ -1,13 +1,13 @@
 import psycopg2
 
-conn = psycopg2.connect("dbname='database1'")
+conn = psycopg2.connect("dbname='db1_py' user='postgres' password='postgres' host='localhost' port=15432")
 cur = conn.cursor()
 
 def create_table():
     cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)")
 
 def insert(item, quantity, price):
-    cur.execute("INSERT INTO store VALUES (?, ?, ?)", (item, quantity, price))
+    cur.execute("INSERT INTO store (item, quantity, price) VALUES (%s, %s, %s)", (item, quantity, price))
 
 def view():
     cur.execute('SELECT * FROM store')
@@ -17,11 +17,15 @@ def view():
 def delete(item):
     # !: "," must be existing after "item"
     # !  when there is only ONE single item in a set
-    cur.execute('DELETE FROM store WHERE item=?', (item,))
+    cur.execute('DELETE FROM store WHERE item=%s', (item,))
 
 def update(item, quantity, price):
-    cur.execute('UPDATE store SET quantity=?, price=? WHERE item=?', (quantity, price, item))
+    cur.execute('UPDATE store SET quantity=%s, price=%s WHERE item=%s', (quantity, price, item))
 
+# create_table()
+# insert('toDelete', 8, 23)
+# delete('toDelete')
+# update('Orange', 40, 13)
 print(view())
 
 conn.commit()
